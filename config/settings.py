4ddad90da05b730080aee9c8b2c7248bbff7b8ab
AKobirMarupov <akobirdilmurodovich@gmail.com>
2026-08-10 10:22:42 +0500

    Read SECRET_KEY and DEBUG from environment

--- config/settings.py
+++ config/settings.py
@@ -13,11 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zbkn#f=h!zvq8)v8b$z-^s!fer8x7$@s&865hppp_j10khdw+2'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 ALLOWED_HOSTS = ['dtm-test.onrender.com', 'localhost', '127.0.0.1']
